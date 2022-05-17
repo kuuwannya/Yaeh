d8f5ddee934423ff9a6e13381a04ad42896b22b8
kuuwannya <kuuuuu05062525@gmail.com>
2022-05-17 19:44:29 +0900

    fix データベース上の検索機能追加,geocorder削除 #11

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,6 @@
 class MapsController < ApplicationController
   def search
-    search_radius = 1.5
+    search_radius = 60
     default_lat = 35.6811673
     default_lng = 139.7670516
     gon.center_of_map_lat = 35.6811673
@@ -11,7 +11,7 @@ class MapsController < ApplicationController
     if params[:q]
       @latitude = geo_params[:latitude].to_f
       @longitude = geo_params[:longitude].to_f
-      @spots = Spot.all.within(search_radius, origin: [@latitude, @longitude]).by_distance(origin: [@latitude, @longitude])
+      @spots = Spot.all.within(search_radius, origin: [@latitude, @longitude])
       @area = search_area(@latitude, @longitude)
       gon.latitude = @latitude
       gon.longitude = @longitude
@@ -45,6 +45,17 @@ class MapsController < ApplicationController
 
   def geo_params
     params.require(:q).permit(:latitude, :longitude)
+  end
+
+  def search_area(lat, long)
+    geoapi_url = "https://geoapi.heartrails.com/api/json?method=searchByGeoLocation&x=#{long}&y=#{lat}"
+    #geoapi_page = URI.open(geoapi_url).read
+    #geoapi_data = JSON.parse(geoapi_page)
+    #prefecture = geoapi_data['response']['location'][0]['prefecture']
+    #city = geoapi_data['response']['location'][0]['city']
+    #town = geoapi_data['response']['location'][0]['town']
+
+    #"#{prefecture}#{city}#{town}"
   end
 
 end
