b69d2684543c05789f477ce2c9e61df9e718023a
kuuwannya <kuuuuu05062525@gmail.com>
2022-06-27 17:25:17 +0900

    add ヘッダー追加・プロフィール画面にユーザー投稿追加 #16

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_one_attached :avatar
+  has_one_attached :header
 
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true, length: { maximum: 255}
